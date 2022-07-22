e4f34f1230525f89a1bfd80d8f5e37be7dd04862
Hank DeDona <hdedona@TRRMLP172.localdomain>
2022-07-22 13:58:06 -0400

    Initial spring boot initializer commit

---
+++ src/main/java/com/notable/health/nhtest/NhTestApplication.java
@@ -0,0 +1,13 @@
+package com.notable.health.nhtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notable/health/nhtest/NhTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notable.health.nhtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
